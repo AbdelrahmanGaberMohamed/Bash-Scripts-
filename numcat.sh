14fc6cbc9717f5fb97c25fb12893fd383d45d856
Abdelrahman <abdelrahmangabermohamed@gmail.com>
2022-03-09 21:53:17 +0200

    added comments to numcat.sh

--- numcat.sh
+++ numcat.sh
@@ -5,21 +5,21 @@
 #	1: File doesn't exist
 #	2: File unreadable 
 FILE=${1}
-if [ ! -f  ${FILE} ]
+if [ ! -f  ${FILE} ]                          ## Check if file exists
 then 
 	echo "No such file"
 	exit 1 
 fi 
-if [ ! -r ${FILE} ]
+if [ ! -r ${FILE} ]							  ## Check if file is readable		
 then 
 	echo "No permission to read this file"
 	exit 2
 fi 
-COUNT=1 
-PAGE=$(cat ${FILE})
-for INDEX in ${PAGE}
+COUNT=1										## Initiate line count =1  									  
+PAGE=$(cat ${FILE})							## Put file content in a variable 			
+for INDEX in ${PAGE}						## Loop over each line 
 do 
-	echo "${COUNT}: ${INDEX}"
-	COUNT=$[COUNT+1]
+	echo "${COUNT}: ${INDEX}"				## print out line number followed by line 
+	COUNT=$[COUNT+1]						## Increasae line count by 1 
 done 
 exit 0
